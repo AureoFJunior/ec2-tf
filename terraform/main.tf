7c1e9742fd81a7583fe9981aecc712b71e80eab5
AureoFJunior <junior7f@hotmail.com>
2022-12-10 15:00:34 -0300

    alter 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = var.ami
-  instance_type = var.instance_typeee
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
